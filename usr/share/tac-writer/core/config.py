d012df9453de65a7c5060aebaab4c9c31d7d8bbd
narayanls <narayan.song@gmail.com>
2026-01-26 19:40:08 -0300

    🐛 fix: .desktop

--- usr/share/tac-writer/core/config.py
+++ usr/share/tac-writer/core/config.py
@@ -13,7 +13,7 @@ class Config:
     """Application configuration manager"""
 
     # Application version and metadata
-    APP_VERSION = "1.21.8"
+    APP_VERSION = "1.21.9"
     APP_NAME = "TAC"
     APP_FULL_NAME = "TAC - Continuous Argumentation Technique"
     APP_DESCRIPTION = "Academic Writing Assistant"
